655d615f2e0f02cbb29c0d256ca66d303dbadc7d
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-10-18 23:40:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # TEST TECH PARA VAGA DE ESTAGIO GETTER - FLEX
+#
+# Autor: Seu Nome 
+# 
 # DESAFIO: 
 #   Crie uma Rotina que busque os dados da url abaixo
 #   https://random-data-api.com/api/app/random_app 
 #   busque 100 registros, salve os dados em um csv
 #   
-
 from requests import get
 import pandas as pd 
 
@@ -16,22 +18,21 @@ def busca_dados(qtd_registros):
     # use GET para buscar e passa como Paramentro 
     # size = 100
     # retorne um dataframe 
-    
     return df
 
-def exporte_sua_base(filename,base):
+def exporte_seu_df(filename,df):
     # filename: base.csv (str) 
-    # base: dataframe (pandas)
+    # df: dataframe (pandas)
     # use o metodo to_csv do pandas
     # para gerar sua base 
     print("... Salvo {} ...".format(filename))
 
 def main():
+  # não precisa alterar essa parte do Código
   print("... Buscando dados ...")
-  base = busca_dados(qtd_registros=100)
+  df = busca_dados(qtd_registros=100)
   print("... Salvando dados ...")
-  exporte_sua_base("base_app.csv",base)
-
+  exporte_seu_df("base_app.csv",df)
 
 if __name__ == "__main__":
      main()
